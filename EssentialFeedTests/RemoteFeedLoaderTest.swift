57c2cbc2b87d7366ee39167b0bd6f18185d4b53f
Amin Faruq <amin.farouk@gits.id>
2022-07-31 00:21:41 +0800

    Initial Commit

---
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -0,0 +1,54 @@
+//
+//  RemoteFeedLoaderTest.swift
+//  EssentialFeedTests
+//
+//  Created by Amin faruq on 28/07/22.
+//
+
+import XCTest
+import EssentialFeed
+
+class RemoteFeedLoaderTest: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL(){
+        let (_, client) = makeSUT()
+                
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadTwice_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url,url])
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+
+        func get(from url: URL) {
+            requestedURLs.append(url)
+        }
+    }
+}
