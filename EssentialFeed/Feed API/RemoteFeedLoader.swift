8aa455790c8c4a0c0c73b30299fa1018d4994a10
Amin Faruq <amin.farouk@gits.id>
2022-08-31 19:09:59 +0800

    add CI Scheme

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -26,6 +26,7 @@ public final class RemoteFeedLoader: FeedLoader {
     public func load(completion: @escaping(Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard self != nil else { return }
+            
             switch result {
             case let .success(data, response):
                 completion(FeedItemsMapper.map(data, from: response))
