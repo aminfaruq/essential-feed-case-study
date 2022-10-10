7ed097c6bb717a6d532f37b4fcf8b70c15f40c2b
Amin Faruq <aminfaruk.fa@gmail.com>
2022-10-10 17:02:07 +0800

    Inject currentDate, locale, and calendar to make tests reliable in any locale

--- EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTest.swift
+++ EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTest.swift
@@ -16,6 +16,8 @@ class ImageCommentsPresenterTest: XCTestCase {
     
     func test_map_createsViewModels() {
         let now = Date()
+        let calendar = Calendar(identifier: .gregorian)
+        let locale = Locale(identifier: "en_US_POSIX")
         
         let comments = [
             ImageComment(
@@ -30,7 +32,12 @@ class ImageCommentsPresenterTest: XCTestCase {
                 username: "another username")
         ]
         
-        let viewModel = ImageCommentsPresenter.map(comments)
+        let viewModel = ImageCommentsPresenter.map(
+            comments,
+            currentDate: now,
+            calendar: calendar,
+            locale: locale
+        )
         
         XCTAssertEqual(viewModel.comments, [
             ImageCommentViewModel(
